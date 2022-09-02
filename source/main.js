fc6d525f40d23bf0b918d815328c7f82b1f4d590
whyisthesheep <77189741+whyisthesheep@users.noreply.github.com>
2022-09-02 14:59:57 +0100

    Add files via upload

---
+++ source/main.js
@@ -0,0 +1,14 @@
+function openLauncher() {
+  var win = window.open()
+  var url = "https://incognito-old.whythesheep.repl.co/"
+  var iframe = win.document.createElement('iframe')
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.border = "none";
+  iframe.src = url
+  win.document.body.appendChild(iframe)
+}
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
